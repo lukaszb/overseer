b9ee1af89fe4d6f956c25b453aa32646292e3ac7
Lukasz Balcerzak <lukaszbalcerzak@gmail.com>
2011-06-27 01:03:31 +0200

    Use django static serve views only if DEBUG mode is turned on

--- overseer/urls.py
+++ overseer/urls.py
@@ -7,14 +7,11 @@ overseer.urls
 """
 
 from django.conf.urls.defaults import *
+from django.conf import settings
 
 import os.path
 
 urlpatterns = patterns('',
-    url(r'^media/(?P<path>.+)?$', 'django.views.static.serve', {
-        'document_root': os.path.join(os.path.dirname(__file__), 'media'),
-        'show_indexes': True
-    }, name='media'),
 
     url(r'^$', 'overseer.views.index', name='index'),
     url(r'^service/(?P<slug>[^/]+)/$', 'overseer.views.service', name='service'),
@@ -25,3 +22,12 @@ urlpatterns = patterns('',
     url(r'^subscription/(?P<ident>[^/]+)/$', 'overseer.views.update_subscription', name='update_subscription'),
     url(r'^subscription/(?P<ident>[^/]+)/verify/$', 'overseer.views.verify_subscription', name='verify_subscription'),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^media/(?P<path>.+)?$', 'django.views.static.serve', {
+            'document_root': os.path.join(os.path.dirname(__file__), 'media'),
+            'show_indexes': True
+        }, name='media'),
+)
+
